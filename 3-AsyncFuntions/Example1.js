3c0aa63a379fdc2d85a905bdfad149b128ef9913
DouglasHdezT <00004516@uca.edu.sv>
2020-02-15 11:10:39 -0600

    Add: Async functions examples

---
+++ 3-AsyncFuntions/Example1.js
@@ -0,0 +1,19 @@
+'use-strict'
+
+/**
+ * Asyncronus call
+ */
+
+const asyncCall = async () => {
+    let value = 5;
+    
+    setTimeout(() => {
+        value += value;
+        console.log(`Result: ${value}`);
+    }, 2000);
+}
+
+console.log("Calling function");
+asyncCall();
+console.log("Message after calling");
+
